2c0637de8bb04e8be0dba6341dc1a9d9cc5656c1
Siri Nandini <sirinandini25@gmail.com>
2026-09-16 01:29:27 +0530

    postgresql://aivoa_k39b_user:EcxovGrFPvkBh6MYGGlh9DbG8VRWh4W0@dpg-dakp5am1egvs738c8r2g-a/aivoa_k39b

--- backend/app/database.py
+++ backend/app/database.py
@@ -4,7 +4,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
 
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+SQLALCHEMY_DATABASE_URL = os.getenv("postgresql://aivoa_k39b_user:EcxovGrFPvkBh6MYGGlh9DbG8VRWh4W0@dpg-dakp5am1egvs738c8r2g-a/aivoa_k39b")
 
 if not SQLALCHEMY_DATABASE_URL:
     raise RuntimeError("DATABASE_URL environment variable is not set")
